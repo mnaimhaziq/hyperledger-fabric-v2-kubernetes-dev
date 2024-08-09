047e992e21a9d84fbff9509baedf914247c247e6
Naim Haziq <101214356+mnaimhaziq@users.noreply.github.com>
2024-08-09 17:05:13 +0800

    Update key-value.go

--- key-value-chaincode/key-value.go
+++ key-value-chaincode/key-value.go
@@ -12,8 +12,16 @@ type KeyValueContract struct {
 	contractapi.Contract
 }
 
+type Asset struct {
+	AppraisedValue int    `json:"AppraisedValue"`
+	Color          string `json:"Color"`
+	ID             string `json:"ID"`
+	Owner          string `json:"Owner"`
+	Size           int    `json:"Size"`
+}
+
 // Create adds a new key with value to the world state
-func (sc *KeyValueContract) Create(ctx contractapi.TransactionContextInterface, key string, value string) error {
+func (sc *KeyValueContract) Create(ctx contractapi.TransactionContextInterface, id string, color string, size int, owner string, appraisedValue int) error {
 	existing, err := ctx.GetStub().GetState(key)
 
 	if err != nil {
@@ -23,8 +31,16 @@ func (sc *KeyValueContract) Create(ctx contractapi.TransactionContextInterface, 
 	if existing != nil {
 		return fmt.Errorf("Cannot create world state pair with key %s. Already exists", key)
 	}
-
-	err = ctx.GetStub().PutState(key, []byte(value))
+	asset := Asset{
+		ID:             id,
+		Color:          color,
+		Size:           size,
+		Owner:          owner,
+		AppraisedValue: appraisedValue,
+	}
+	assetJSON, err := json.Marshal(asset)
+	
+	err = ctx.GetStub().PutState(id, assetJSON)
 
 	if err != nil {
 		return errors.New("Unable to interact with world state")
