7f7eca17e3dec1e22dd4c2c93f9222cad166301c
Naim Haziq <101214356+mnaimhaziq@users.noreply.github.com>
2024-08-09 21:24:36 +0800

    Update main.go

--- key-value-chaincode/main.go
+++ key-value-chaincode/main.go
@@ -1,19 +1,23 @@
+/*
+SPDX-License-Identifier: Apache-2.0
+*/
+
 package main
 
 import (
+	"log"
+
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+	"github.com/hyperledger/fabric-samples/asset-transfer-basic/chaincode-go/chaincode"
 )
 
 func main() {
-	keyValueContract := new(KeyValueContract)
-
-	cc, err := contractapi.NewChaincode(keyValueContract)
-
+	assetChaincode, err := contractapi.NewChaincode(&chaincode.SmartContract{})
 	if err != nil {
-		panic(err.Error())
+		log.Panicf("Error creating asset-transfer-basic chaincode: %v", err)
 	}
 
-	if err := cc.Start(); err != nil {
-		panic(err.Error())
+	if err := assetChaincode.Start(); err != nil {
+		log.Panicf("Error starting asset-transfer-basic chaincode: %v", err)
 	}
 }
